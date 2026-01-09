9427750784717f5a44be241cae232da8a8a0df90
Redooyyy <rwarieal@gmail.com>
2026-01-09 20:40:40 +0600

    Fixing something

--- lua/flutter_deps/init.lua
+++ lua/flutter_deps/init.lua
@@ -19,69 +19,49 @@ function M.add_dependency()
   local finder = require('flutter_deps.finder')
   local writer = require('flutter_deps.writer')
 
-  -- Ask user for query
-  local query = vim.fn.input('Search pub.dev package: ')
-
-  if query == '' then
-    print('No package name provided')
+  local has_telescope, telescope = pcall(require, 'telescope')
+  if not has_telescope then
+    print('Telescope.nvim is required for this plugin')
     return
   end
 
-  -- Fetch matching packages
-  local results = finder.search(query)
+  local pickers = require('telescope.pickers')
+  local finders = require('telescope.finders')
+  local conf = require('telescope.config').values
+  local actions = require('telescope.actions')
+  local action_state = require('telescope.actions.state')
 
-  if #results == 0 then
-    print('No results found for: ' .. query)
-    return
-  end
-
-  local selection
-
-  if config.use_telescope then
-    local has_telescope, telescope = pcall(require, 'telescope')
-    if not has_telescope then
-      print('Telescope not found! Showing first result')
-      selection = results[1]
-    else
-      local pickers = require('telescope.pickers')
-      local finders = require('telescope.finders')
-      local conf = require('telescope.config').values
-      local actions = require('telescope.actions')
-      local action_state = require('telescope.actions.state')
-
-      pickers
-        .new({}, {
-          prompt_title = 'Pub.dev Packages',
-          finder = finders.new_table({
-            results = results,
-            entry_maker = function(entry)
-              return {
-                value = entry,
-                display = entry.name .. ' [' .. entry.latest_version .. ']',
-                ordinal = entry.name,
-              }
-            end,
-          }),
-          sorter = conf.generic_sorter({}),
-          attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-              selection = action_state.get_selected_entry().value
-              actions.close(prompt_bufnr)
-            end)
-            return true
-          end,
-        })
-        :find()
-    end
-  else
-    -- Just pick the first one if Telescope is disabled
-    selection = results[1]
-  end
-
-  if selection then
-    writer.add_to_pubspec(selection.name, selection.latest_version)
-    print('Added ' .. selection.name .. '@' .. selection.latest_version .. ' to pubspec.yaml')
-  end
+  -- Open Telescope directly with a dynamic finder
+  pickers
+    .new({}, {
+      prompt_title = 'Search pub.dev packages',
+      finder = finders.new_dynamic({
+        fn = function(input)
+          if input == '' then
+            return {}
+          end
+          return finder.search(input) -- search pub.dev dynamically as user types
+        end,
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            display = entry.name .. ' [' .. entry.latest_version .. ']',
+            ordinal = entry.name,
+          }
+        end,
+      }),
+      sorter = conf.generic_sorter({}),
+      attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+          local selection = action_state.get_selected_entry().value
+          actions.close(prompt_bufnr)
+          writer.add_to_pubspec(selection.name, selection.latest_version)
+          print('Added ' .. selection.name .. '@' .. selection.latest_version)
+        end)
+        return true
+      end,
+    })
+    :find()
 end
 
 M.config = config
